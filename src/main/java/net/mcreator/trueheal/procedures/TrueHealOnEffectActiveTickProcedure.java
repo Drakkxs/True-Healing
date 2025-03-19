1c293b7c166c8f75910d550ec723d0aef60ea810
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-03-19 03:40:32 -0400

    Players can no longer get the benifits of the true health effect even if they gain it from creeper explosions.

--- src/main/java/net/mcreator/trueheal/procedures/TrueHealOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/trueheal/procedures/TrueHealOnEffectActiveTickProcedure.java
@@ -1,5 +1,6 @@
 package net.mcreator.trueheal.procedures;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 
@@ -7,7 +8,7 @@ public class TrueHealOnEffectActiveTickProcedure {
 	public static void execute(Entity entity, double amplifier) {
 		if (entity == null)
 			return;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1) > 0) {
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1) > 0 && !(entity instanceof Player)) {
 			if (entity instanceof LivingEntity _entity)
 				_entity.setHealth((float) Math.min((entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1) + amplifier, entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1));
 		}
