ac4a2def006d6eca14df7312361c650a675c80d3
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-10-30 22:07:48 -0400

    Update PreHeal removal to properly work.

--- src/main/java/net/mcreator/trueheal/procedures/TrueHealEnvironmentProcedure.java
+++ src/main/java/net/mcreator/trueheal/procedures/TrueHealEnvironmentProcedure.java
@@ -34,8 +34,6 @@ public class TrueHealEnvironmentProcedure {
 		if (world.getLevelData().getGameRules().getBoolean(TruehealModGameRules.TRUEHEALCD)) {
 			if (entity instanceof LivingEntity _entity)
 				_entity.removeEffect(TruehealModMobEffects.TRUE_HEAL.get());
-			if (entity instanceof LivingEntity _entity)
-				_entity.removeEffect(TruehealModMobEffects.PRE_HEAL.get());
 		}
 		if (TrueHealEntityValidationProcedure.execute(entity)) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
