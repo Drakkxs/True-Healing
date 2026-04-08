5ced075f5b148f60c53727a5d74a6019da7eef77
Drakkxs <70417194+Drakkxs@users.noreply.github.com>
2026-04-07 20:43:57 -0400

    Added config! Yaya.

--- src/main/java/net/mcreator/trueheal/procedures/TrueHealEntityValidationProcedure.java
+++ src/main/java/net/mcreator/trueheal/procedures/TrueHealEntityValidationProcedure.java
@@ -1,15 +1,16 @@
 package net.mcreator.trueheal.procedures;
 
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 
 public class TrueHealEntityValidationProcedure {
-	public static boolean execute(Entity entity) {
-		if (entity == null)
+	public static boolean execute(Entity entityToCheck) {
+		if (entityToCheck == null)
 			return false;
-		if (entity instanceof Player) {
-			return false;
+		if (TrueHealGetSettingIgnoreListProcedure.execute()) {
+			return true;
+		} else if (TrueHealGetSettingUseWhitelistProcedure.execute()) {
+			return TrueHealCheckWhitelistProcedure.execute(entityToCheck);
 		}
-		return true;
+		return !TrueHealCheckBlacklistProcedure.execute(entityToCheck);
 	}
 }
---
+++ src/main/java/net/mcreator/trueheal/procedures/TrueHealGetSettingIgnoreListProcedure.java
@@ -0,0 +1,36 @@
+package net.mcreator.trueheal.procedures;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+public class TrueHealGetSettingIgnoreListProcedure {
+	public static boolean execute() {
+		File truehealconfig = new File("");
+		com.google.gson.JsonObject mainObj = new com.google.gson.JsonObject();
+		com.google.gson.JsonObject subObj = new com.google.gson.JsonObject();
+		com.google.gson.JsonArray blacklist = new com.google.gson.JsonArray();
+		com.google.gson.JsonArray whitelist = new com.google.gson.JsonArray();
+		boolean use_whitelist = false;
+		boolean ignore_list = false;
+		truehealconfig = TrueHealGetConfigProcedure.execute();
+		{
+			try {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(truehealconfig));
+				StringBuilder jsonstringbuilder = new StringBuilder();
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					jsonstringbuilder.append(line);
+				}
+				bufferedReader.close();
+				mainObj = new com.google.gson.Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+				subObj = mainObj.get("settings").getAsJsonObject();
+				ignore_list = subObj.get("ignore_list").getAsBoolean();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return ignore_list;
+	}
+}
